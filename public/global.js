37e9aa570531237b6ecae5ffb782ff78c9350f86
Rachel VanBoening <vanboeningrr@gmail.com>
2015-03-09 18:20:00 -0500

    Works with all slides and buttons

--- public/global.js
+++ public/global.js
@@ -1,44 +1,68 @@
-//
-// var current_slide = 1;
-//
-// window.onload = function(){
-//
-//   document.getElementById("slide_show").onclick = loadSlide
-//   function loadSlide() {
-//     var js_req = new XMLHttpRequest;
-//     js_req.addEventListener("load", function() {
-//       var parsed_response = JSON.parse(js_req.response);
-//       // console.log(parsed_response[0].title);
-//       document.getElementById("result_title").innerHTML = parsed_response[0].title;
-//       document.getElementById("result_body").innerHTML = parsed_response[0].body;
-//
-//     });
-//     js_req.open("get", "http://localhost:4567/allslides");
-//     js_req.send();
-//   }
-// }
-//
-
-
-var current_slide = 1;
+var parsed_response;
 
 window.onload = function(){
 
-  document.getElementById("slide_show2").onclick = loadSlide
+  document.getElementById("slide_show").onclick = loadSlide
   function loadSlide() {
-      var js_req = new XMLHttpRequest;
+    var js_req = new XMLHttpRequest;
     js_req.addEventListener("load", function() {
-      console.log(js_req);
-      var parsed_response = JSON.parse(js_req.response);
+      parsed_response = JSON.parse(js_req.response);
       // console.log(parsed_response[0].title);
-      document.getElementById("result_title").innerHTML = parsed_response.title;
-      document.getElementById("result_body").innerHTML = parsed_response.body;
+      document.getElementById("result_title").innerHTML = parsed_response[0].title;
+      document.getElementById("result_body").innerHTML = parsed_response[0].body;
 
     });
-    js_req.open("get", "http://localhost:4567/oneslide");
+    js_req.open("get", "http://localhost:4567/allslides");
     js_req.send();
   }
+  
+  var i = 0;
+  
+  document.getElementById("next_slide").onclick = function() {
+    if (i < 4) {
+  i++; 
+  console.log(parsed_response)
+  document.getElementById("result_title").innerHTML = parsed_response[i].title;
+  document.getElementById("result_body").innerHTML = parsed_response[i].body;
+  }
 }
+  
+  document.getElementById("last_slide").onclick = function() {
+    if (i > 0) {
+  i--; 
+  console.log(parsed_response)
+  document.getElementById("result_title").innerHTML = parsed_response[i].title;
+  document.getElementById("result_body").innerHTML = parsed_response[i].body;
+  }
+}
+};
+
+//
+//
+// var current_slide = 1;
+//
+// var next_slide = current_slide + 1
+//
+// var last_slide = current_slide -1
+//
+// window.onload = function(){
+//
+//   document.getElementById("slide_show2").onclick = loadSlide
+//   function loadSlide() {
+//       var js_req = new XMLHttpRequest;
+//     js_req.addEventListener("load", function() {
+//       console.log(js_req);
+//       var parsed_response = JSON.parse(js_req.response);
+//       // console.log(parsed_response[0].title);
+//       document.getElementById("result_title").innerHTML = parsed_response.title;
+//       document.getElementById("result_body").innerHTML = parsed_response.body;
+//
+//     });
+//     js_req.open("get", "http://localhost:4567/oneslide/:id");
+//     js_req.send();
+//   }
+//
+// }
 
 
  
