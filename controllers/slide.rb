3dc2fb132ab735ecbaa69eb7876ea6f166e3b27b
Rachel VanBoening <vanboeningrr@gmail.com>
2015-03-27 11:17:03 -0500

    converted Slide methods to active record

--- controllers/slide.rb
+++ controllers/slide.rb
@@ -1,17 +1,16 @@
 
 get '/slides' do
-  binding.pry
   erb :homepage
 end
 
 get '/allslides' do
-  all_slides=Slide.fetch_all # this returns an array of hashes
+  all_slides=Slide.all # this returns an array of hashes
   all_slides.to_json
 end
 
 
 get '/oneslide/:id' do 
-  one_slide = Slide.fetch_slide[params(:id)]
+  one_slide = Slide.find_by[params(:id)]
   one_slide.to_json
 end
 
