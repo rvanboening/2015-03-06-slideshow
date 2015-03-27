400383e596ffb9957640e1022a0a5684b1773962
Rachel VanBoening <vanboeningrr@gmail.com>
2015-03-27 11:36:28 -0500

    Added user with activerecord

--- main.rb
+++ main.rb
@@ -11,6 +11,6 @@ DATABASE = SQLite3::Database.new("database/slideshow.db")
 
 require_relative "database/database_setup.rb"
 require_relative "models/slide.rb"
-#require_relative "models/user.rb"
+require_relative "models/user.rb"
 require_relative "controllers/slide.rb"
-#require_relative "controllers/user.rb"
+require_relative "controllers/user.rb"
