027a2dfa01460a6d8469f1a4f224e476365d3a1c
Rachel VanBoening <vanboeningrr@gmail.com>
2015-03-27 11:15:39 -0500

    Added ActiveRecord

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
-# require "sinatra/activerecord"
+require "sinatra/activerecord"
 require "sinatra"
 require 'json'
 require "sqlite3"
 require "pry"
 require 'bcrypt'
 
-#set :database, {adapter: "sqlite3", database: "database/slideshow.db"}
+set :database, {adapter: "sqlite3", database: "database/slideshow.db"}
 
 DATABASE = SQLite3::Database.new("database/slideshow.db")
 
